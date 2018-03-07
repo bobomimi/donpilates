437593abee68a552b515df92fbf7e73b652edf15
ben leer <boaz242@gmail.com>
2018-03-07 14:26:44 -0600

    init

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+  	@posts = Post.all
+  	@products = Product.all
+  end
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    devise_for :admins
     root 'welcome#index'
+    get 'welcome/about'
+    resources :contacts, only: [:new, :create]
     resources :products
     resources :posts
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
