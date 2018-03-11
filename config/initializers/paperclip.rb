316409cee0ff30c51cd30e9737858c4f598a1c1c
ben leer <boaz242@gmail.com>
2018-03-12 01:26:45 +0200

    init

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
-Paperclip::Attachment.default_options[:url] = 'cdn.donpilates.com'
+Paperclip::Attachment.default_options[:url] = 'd1egmt44b887qe.cloudfront.net'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
