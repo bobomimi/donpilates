a3f2fbf55a368a3bb69207cc95feb404aa7abba8
ben leer <boaz242@gmail.com>
2018-03-12 08:04:19 +0200

    :init:

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,13 @@ class Product < ApplicationRecord
       attribute_changed?('title')
     end
      
-    has_attached_file :image, default_url: "/images/:style/missing.png"
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }, default_url: "/images/:style/missing.png"
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
