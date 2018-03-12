e102d2998242b5e8bcaa58adc2071deafaad3ec8
ben leer <boaz242@gmail.com>
2018-03-12 07:24:27 +0200

    init

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,2 @@
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 'd1egmt44b887qe.cloudfront.net'
