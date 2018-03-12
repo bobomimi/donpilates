524d27f11795bf60c176288fdd5fc868be195efd
ben leer <boaz242@gmail.com>
2018-03-12 07:38:04 +0200

    init

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,7 +12,5 @@ class Product < ApplicationRecord
      
     has_attached_file :image, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-    
-
 
 end
