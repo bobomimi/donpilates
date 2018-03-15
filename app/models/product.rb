946d0f9e9b4eaba843e2d100afe09519bc1f8c93
ben leer <boaz242@gmail.com>
2018-03-15 02:16:44 +0200

    init

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,15 +14,15 @@ class Product < ApplicationRecord
 
   
   has_attached_file :image,
-      :default_url => "/images/:style/missing.png",
-      :storage => :s3,
-      :s3_credentials => {
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] },
-      :url => ':s3_alias_url',
-      :s3_host_alias => 'd1egmt44b887qe.cloudfront.net', 
-      :bucket => 'danielpilates',
-      s3_headers: { 'Expires' => 1.hour.from_now.httpdate }
+      :default_url => "/images/:style/missing.png"
+      #:storage => :s3,
+      #:s3_credentials => {
+      #  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+     #   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] },
+     # :url => ':s3_alias_url',
+     # :s3_host_alias => 'd1egmt44b887qe.cloudfront.net', 
+     # :bucket => 'danielpilates',
+     # s3_headers: { 'Expires' => 1.hour.from_now.httpdate }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
