3c5c72f7d92380ca2c8ebdabb6f70df09794ee30
ben leer <boaz242@gmail.com>
2018-03-21 14:46:08 +0200

    o

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
     }
   }
 
-  #config.action_controller.asset_host = 'd36y0kv3zyrf84.cloudfront.net'
+  config.action_controller.asset_host = 'd3t19itc2fbau0.cloudfront.net'
 
   config.action_controller.perform_caching = true
   
